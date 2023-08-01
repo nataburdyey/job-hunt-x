3e63dd36118a7ef68fcf1e252e30956c0890f5fa
Natalia Burdyey <nata.burdey@gmail.com>
2023-07-31 20:22:52 -0400

    navigate to dashboard after login

--- src/features/user/userSlice.jsx
+++ src/features/user/userSlice.jsx
@@ -13,7 +13,7 @@ export const registerUser = createAsyncThunk(
   'user/registerUser',
   async (user, thunkAPI) => {
     try {
-      const resp = await customFetch.post('/auth/testingRegister', user);
+      const resp = await customFetch.post('/auth/register', user);
       return resp.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.response.data.msg);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,6 +6,7 @@ import Wrapper from '../assets/wrappers/RegisterPage';
 import Logo from '../components/Logo';
 import FormRow from '../components/FormRow';
 import { loginUser, registerUser } from '../features/user/userSlice';
+import { useNavigate } from 'react-router-dom';
 
 const initialState = {
   name: '',
@@ -19,6 +20,7 @@ const Register = () => {
 
   const { user, isLoading } = useSelector((store) => store.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -44,6 +46,14 @@ const Register = () => {
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
+
+  useEffect(() => {
+    if (user){
+      setTimeout(()=>{
+        navigate('/')
+      }, 2000)
+    }
+  }, [user]);
 
   return (
     <Wrapper className='full-page'>
@@ -71,7 +81,7 @@ const Register = () => {
           handleChange={handleChange}
         />
         <button type='submit' className='btn btn-block' disabled={isLoading}>
-         {isLoading ? "Loading..." : "Submit"}
+          {isLoading ? 'Loading...' : 'Submit'}
         </button>
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member'}
