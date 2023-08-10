dd705e405c175c62aacb330b0790265042b6435e
Natalia Burdyey <nata.burdey@gmail.com>
2023-08-10 13:08:54 -0400

    all jobs page setup

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,21 @@
 import BigSidebar from './BigSidebar';
-import Logo from "./Logo";
+import Logo from './Logo';
 import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import FormRow from './FormRow';
 import FormRowSelect from './FormRowSelect';
+import JobsContainer from './JobsContainer';
+import Job from './Job';
+import SearchContainer from './SearchContainer';
 
-export { Logo, BigSidebar, Navbar, SmallSidebar, FormRow, FormRowSelect };
+export {
+  Logo,
+  BigSidebar,
+  Navbar,
+  SmallSidebar,
+  FormRow,
+  FormRowSelect,
+  JobsContainer,
+  Job,
+  SearchContainer,
+};
