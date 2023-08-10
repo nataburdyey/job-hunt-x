68ecbd2305de586d8d033329d596fb6da7270c4f
Natalia Burdyey <nata.burdey@gmail.com>
2023-08-10 13:26:13 -0400

    container setup

--- src/components/JobsContainer.jsx
+++ src/components/JobsContainer.jsx
@@ -1,6 +1,35 @@
+import { useEffect } from 'react';
+import Job from './Job';
+import Wrapper from '../assets/wrappers/JobsContainer';
+import { useSelector, useDispatch } from 'react-redux';
+import Loading from './Loading';
+
 const JobsContainer = () => {
+  const { jobs, isLoading } = useSelector((store) => store.allJobs);
+  if (isLoading) {
+    return (
+      <Wrapper>
+        <Loading center/>
+      </Wrapper>
+    );
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display...</h2>
+      </Wrapper>
+    );
+  }
   return (
-    <div>JobsContainer</div>
-  )
-}
-export default JobsContainer
+    <Wrapper>
+      <h5>Jobs Info</h5>
+      <div className='jobs'>
+        {jobs.map((job) => {
+          return <Job key={job._id} {...job} />;
+        })}
+      </div>
+    </Wrapper>
+  );
+};
+export default JobsContainer;
